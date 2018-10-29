ce60758ba69ed72d10da4aa451ce4ddbfe3dc74a
LAMPE Ronan <lampe.ronan@outlook.fr>
2018-10-29 22:39:23 +0100

    Fin du passage en OO & Sécurisation des paramètres
    Transition vers l'OO terminée + Ajout d'une sécurité sur les paramètres du script + Autres..

--- simCache160.py
+++ simCache160.py
@@ -14,8 +14,33 @@ import os
 
 
 # ----------------------------------------------------------------------
+# ----------------------- VARIABLES INDICATEURS ------------------------
+# ----------------------------------------------------------------------
+
+# Nb tot de lectures
+tot_lec = 0
+# Nb tot d'écritures
+tot_ecr = 0
+# Nb défauts en lecture
+def_lec = 0
+# Nb défauts en écriture
+def_ecr = 0
+
+
+# ----------------------------------------------------------------------
 # ----------------------------- CLASSES --------------------------------
 # ----------------------------------------------------------------------
+
+# Classe d'un bloc mémoire
+class Bloc:
+    # Procédure d'initialisation
+    def __init__(self):
+        #
+        self.valide = bool
+
+        # Etiquette
+        self.tag = int
+
 
 # Classe de la mémoire cache
 class Memoire:
@@ -23,57 +48,37 @@ class Memoire:
     # ------------------------- VARIABLES MEMOIRE --------------------------
     # ----------------------------------------------------------------------
 
-    # Cache size (taille totale de la cache, octet)
-    cs = 4096
-
-    # Block size (taille d'un bloc, octet)
-    bs = 64
-
-    # Degré d'associativité du cache
-    assoc = 4
-
-    # Chemin du fichier de trace mémoire (nom de la trace si même dossier source)
-    trace_mem = ""
-
-    # Nombre d'ensembles
-    nbe = int(cs / (bs * assoc))
-
-    #
-    # numbloc = adr / bs
-
-    # Index
-    # index = numbloc % nbe
-
-    # Etiquette
-    # tag = numbloc / nbe
+    # Cache (A INITIALISER)
+    cache = list
 
     # ----------------------------------------------------------------------
     # --------------------------- PROCEDURES -------------------------------
     # ----------------------------------------------------------------------
 
-    # Vérification de la récupération des paramètres (& affectation)
-    def verif_parametrage(self, arguments):
-        if len(arguments) == 4:
-            self.cs = int(arguments[0])
-            print("Taille de la cache.. " + str(self.cs) + " octets")
+    # Procédure d'initialisation
+    def __init__(self, params):
+        # Cache size (taille totale de la cache, octet)
+        self.cs = params[0]
 
-            self.bs = int(arguments[1])
-            print("Taille d'un bloc.. " + str(self.bs) + " octets")
+        # Block size (taille d'un bloc, octet)
+        self.bs = params[1]
 
-            self.assoc = int(arguments[2])
-            print("Degré d'associativité.. " + str(self.assoc))
+        # Degré d'associativité du cache
+        self.assoc = params[2]
 
-            if os.path.exists(arguments[3]):
-                self.trace_mem = arguments[3]
-                print("Trace mémoire.. " + self.trace_mem + " (trouvé)")
-            else:
-                print("Erreur: Trace mémoire introuvable")
-                exit(1)
+        # Chemin du fichier de trace mémoire (nom de la trace si même dossier source)
+        self.trace_mem = params[3]
 
-            self.nbe = int(self.cs / (self.bs * self.assoc))
-        else:
-            print("Erreur: " + str(4 - len(arguments)) + " Paramètres manquants")
-            exit(1)
+        # Nombre d'ensembles
+        self.nbe = int(self.cs / (self.bs * self.assoc))
+
+    # Affichage des paramètres récupérés
+    def affiche_params(self):
+        print("Taille de la cache.. " + str(self.cs) + " octets")
+        print("Taille d'un bloc.. " + str(self.bs) + " octets")
+        print("Degré d'associativité.. " + str(self.assoc))
+        print("Nombre d'ensembles.. " + str(self.nbe))
+        print("Trace mémoire.. " + os.path.abspath(self.trace_mem))
 
     # Recherche du type du cache
     def type_cache(self):
@@ -85,13 +90,27 @@ class Memoire:
         else:
             print("Cache de type inconnu ou erreur de paramétrage")
 
-    # Lecture de la trace
+    # Lecture de la trace (EN CONSTRUCTION)
     def lecture_trace(self):
         print("\nOuverture de la trace mémoire...")
         with open(self.trace_mem, "r") as trace:
             print("Récupération du contenu de la trace mémoire..")
             for inst in trace.readlines():
-                pass
+
+                # Numéro de bloc
+                numbloc = int(int(inst[1:], 16) / self.bs)
+
+                # Index
+                index = numbloc % self.nbe
+
+                # Etiquette
+                tag = int(numbloc / self.nbe)
+
+                assoc = 0
+                trouve = False
+                while assoc < self.assoc and not trouve:
+                    if not cache[index][assoc].valide or cache[index][assoc].tag != tag:
+                        assoc += 1
         print("La trace mémoire a bien été lue")
 
     # Random
@@ -107,18 +126,50 @@ class Memoire:
         pass
 
 
-# ----------------------------------------------------------------------
-# ----------------------- VARIABLES INDICATEURS ------------------------
-# ----------------------------------------------------------------------
+# Vérification de la récupération des paramètres (& affectation)
+def verif_parametrage():
+    # Récupération des arguments passés au script
+    print("\nLecture des paramètres...")
+    arguments = sys.argv[1:]
+    param_ok = True
 
-# Nb tot de lectures
-tot_lec = 0
-# Nb tot d'écritures
-tot_ecr = 0
-# Nb défauts en lecture
-def_lec = 0
-# Nb défauts en écriture
-def_ecr = 0
+    # On vérifie si on récupère bien 4 arguments
+    if len(arguments) == 4:
+        cs = arguments[0]
+        bs = arguments[1]
+        assoc = arguments[2]
+        trace_mem = arguments[3]
+
+        # On vérifie si le cache size est du bon type
+        if not str.isdigit(cs):
+            print("Erreur: Le type du paramètre \"cs\" est invalide")
+            param_ok = False
+
+        # On vérifie si le block size est du bon type
+        if not str.isdigit(bs):
+            print("Erreur: Le type du paramètre \"bs\" est invalide")
+            param_ok = False
+
+        # On vérifie si le degré d'associativité est du bon type
+        if not str.isdigit(assoc):
+            print("Erreur: Le type du paramètre \"assoc\" est invalide")
+            param_ok = False
+
+        # On vérifie si le fichier de trace mémoire existe
+        if not os.path.exists(trace_mem):
+            print("Erreur: Fichier de trace mémoire introuvable")
+            param_ok = False
+
+        # Si les paramètres sont valides -> on retourne la liste des arguments castés
+        # Sinon -> On arrête le script
+        if param_ok:
+            return [int(cs), int(bs), int(assoc), trace_mem]
+        else:
+            exit(1)
+    else:
+        print("Erreur: " + str(4 - len(arguments)) + " paramètres manquants")
+        exit(1)
+
 
 # ----------------------------------------------------------------------
 # -------------------------- MAIN DU SCRIPT ----------------------------
@@ -128,18 +179,12 @@ print(" -------------------------- ")
 print("| SIMULATION MEMOIRE CACHE |")
 print(" -------------------------- ")
 
-# Récupération des arguments passés au script
-print("\nLecture des paramètres...")
-arguments = sys.argv[1:]
-
 # On crée notre instance de mémoire cache
-MemCache = Memoire()
-
-# Vérification du paramétrage
-MemCache.verif_parametrage(arguments)
+MemCache = Memoire(verif_parametrage())
+MemCache.affiche_params()
 
 # Recherche du type de mémoire cache
 MemCache.type_cache()
 
 # Lecture de la trace mémoire
-MemCache.lecture_trace()
+# MemCache.lecture_trace()
